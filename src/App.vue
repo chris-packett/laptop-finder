13830edd7558468fbd39bbd366b3699996d6e74c
Chris <chrispackett1998@gmail.com>
2018-12-02 13:56:03 -0500

    basic filterable table

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to our Laptop Finder App!"/>
+    <br>
+    <input type="text" v-model="searchInput">
+    <br>
+    <br>
+    <div class="laptop-table">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Laptop</th>
+            <th scope="col">Rating</th>
+            <th scope="col">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="laptop in filteredLaptops" :key="laptop.id">
+            <td>{{ laptop.name }}</td>
+            <td>{{ laptop.rating }}</td>
+            <td>${{ laptop.price }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -12,6 +33,23 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data: function() {
+    return {
+      searchInput: '',
+      laptops: [
+        { id: 1, name: 'Thing 1', rating: '3.7', price: 100 },
+        { id: 2, name: 'Thing 2', rating: '4.1', price: 80 },
+        { id: 3, name: 'Thing 3', rating: '3.0', price: 180 }
+      ]
+    }
+  },
+  computed: {
+    filteredLaptops() {
+      return this.laptops.filter(laptop => {
+        return (laptop.name.toLowerCase().includes(this.searchInput.toLowerCase()));
+      })
+    }
   }
 }
 </script>
@@ -24,5 +62,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.laptop-table {
+  width: 75%;
+  margin: auto;
 }
 </style>
